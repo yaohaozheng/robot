223031748c9f1c731789af47a08b5c7ea2e3cba5
tanqy <tan_qy@outlook.com>
2018-12-18 16:53:14 +0800

    添加简单test

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,19 +1,21 @@
 class SessionsController < ApplicationController
   include SessionsHelper
 
+  def new
+  end
+  
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
       params[:session][:remember_me] == '1' ? remember_user(user) : forget_user(user)
-      flash= {:info => "欢迎回来: #{user.name} :)"}
+      flash.now[:success] = '欢迎回来: #{user.name} :)'
+      redirect_to user
     else
-      flash= {:danger => '账号或密码错误'}
+      flash.now[:danger] = '无效的账号或密码'
+      render 'new'
     end
-    redirect_to user, :flash => flash
-  end
-
-  def new
+    
   end
 
   def destroy
